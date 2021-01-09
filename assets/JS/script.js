6cf6cfd01a8037459792876f2b8ffbc11a280787
emilykidd3 <e.kidd61@yahoo.com>
2021-01-08 20:39:33 -0800

    added colors for past present or future in text area

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,40 @@
 var hourDivs= document.getElementsByClassName("hour");
-
 var DateTime = luxon.DateTime;
-
-var now = DateTime.local();
-
 var militaryHour = DateTime.local().toFormat("HH");
 
+var schedule = {};
+
+var list = JSON.parse(localStorage.getItem('schedule'))
+
 var currentDate = DateTime.local().toFormat("cccc',' LLLL d");
+$("#currentDay").text(currentDate);
 
-for (var i=0; i<hourDivs.length; i++) {
-    console.log(hourDivs[i].getAttribute('id'))
-};
+$(".description").each(function(){
+    if (parseInt($(this).attr("id")) < militaryHour) {
+        $(this).removeClass("future present")
+        $(this).addClass("past")
+    }
+    else if (parseInt($(this).attr("id")) === militaryHour) {
+        $(this).removeClass("past future")
+        $(this).addClass("present")
+    }
+    else {
+        $(this).removeClass("present past")
+        $(this).addClass("future")
+    }
+})
 
-console.log(militaryHour);
+var loadSchedule = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+}
+
+$('.saveBtn').on("click", function() {
+    console.log("saved")
+    
+})
+
+var saveSchedule = function() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+loadSchedule();
